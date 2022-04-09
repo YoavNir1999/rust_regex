14052b4d7b56defe6a70eb44f91194b61d44fd2f
yoav nir <yoavnir@yoavs-MacBook-Air.local>
2022-04-09 16:06:57 +0300

    uncommented code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod functions;
 use functions::*;
 
 fn main() {
-    //let x = find_pattern(String::from("llo"),String::from("hello world"));
-    //println!("{}",x);
+    let x = find_pattern(String::from("llo"),String::from("hello world"));
+    println!("{}",x);
     println!("{:?}",return_char_type("3".to_owned()));
 }
